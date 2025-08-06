997d650030451e32023cb2e06b5b25075f6c51c3
Romario Queiroz <romarioqueiroz013@gmail.com>
2025-08-06 16:56:38 -0300

    parte 01

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <span>
+          <div>
+            <h1>NOSSA VOZ</h1>
+            <h2>Legenda </h2>
+          </div>
+
+          <div>
+            <h3>lorem ipsum</h3>
+            <h3>lorem ipsum</h3>
+            <h3>lorem ipsum</h3>
+            <h3>lorem ipsum</h3>
+            <h3>lorem ipsum</h3>
+          </div>
+        </span>
+      </header>
+      <body>
+        <span>
+          <div>
+            <h3>Ponte dos Ingleses</h3>
+            <h4>Turismo, lazer e cidadania</h4>
+            <p>
+              At Lorem Ipsum, our team is a diverse mix of creatives, tinkerers,
+              developers, designers, hackers, and tech enthusiasts. Just like
+              the classic placeholder text, ‘Lorem Ipsum,’ which represents the
+              blend of structure and creativity, we embody that spirit by
+              crafting innovative solutions that balance form and function. We
+              thrive on inventing and building with a strong creative vision,
+              ensuring that optimization, security, and usability are always
+              non-negotiable.
+            </p>
+            <button>Ler mais</button>
+          </div>
+        </span>
+        <span>
+          <h3>A volta da Ponte</h3>
+          <h4>Ícone cultural da cidade</h4>
+          <p>
+            At Lorem Ipsum, our team is a diverse mix of creatives, tinkerers,
+            developers, designers, hackers, and tech enthusiasts. Just like the
+            classic placeholder text, ‘Lorem Ipsum,’ which represents the blend
+            of structure and creativity, we embody that spirit by crafting
+            innovative solutions that balance form and function. We thrive on
+            inventing and building with a strong creative vision, ensuring that
+            optimization, security, and usability are always non-negotiable.
+          </p>
+          <button>Ler mais</button>
+        </span>
+        <span>
+          <h3>Sanfonas</h3>
+          <h4>O som do coração</h4>
+          <p>
+            At Lorem Ipsum, our team is a diverse mix of creatives, tinkerers,
+            developers, designers, hackers, and tech enthusiasts. Just like the
+            classic placeholder text, ‘Lorem Ipsum,’ which represents the blend
+            of structure and creativity, we embody that spirit by crafting
+            innovative solutions that balance form and function. We thrive on
+            inventing and building with a strong creative vision, ensuring that
+            optimization, security, and usability are always non-negotiable.
+          </p>
+          <button>Ler mais</button>
+        </span>
+        <span>
+          <h3>Bodegas</h3>
+          <h4>Tradição & resiliência</h4>
+          <p>
+            At Lorem Ipsum, our team is a diverse mix of creatives, tinkerers,
+            developers, designers, hackers, and tech enthusiasts. Just like the
+            classic placeholder text, ‘Lorem Ipsum,’ which represents the blend
+            of structure and creativity, we embody that spirit by crafting
+            innovative solutions that balance form and function. We thrive on
+            inventing and building with a strong creative vision, ensuring that
+            optimization, security, and usability are always non-negotiable.
+          </p>
+          <button>Ler mais</button>
+        </span>
+        <span>
+          <div>
+            <button>Ver todas as matérias</button>
+          </div>
+        </span>
+      </body>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
